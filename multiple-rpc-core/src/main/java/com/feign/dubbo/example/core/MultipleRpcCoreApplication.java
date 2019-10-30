8f10cd1d4631d10585702f875425be77c7244d12
ChenHanLin <925576956@qq.com>
2019-10-30 16:34:29 +0800

    init projects

---
+++ multiple-rpc-core/src/main/java/com/feign/dubbo/example/core/MultipleRpcCoreApplication.java
@@ -0,0 +1,13 @@
+package com.feign.dubbo.example.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleRpcCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleRpcCoreApplication.class, args);
+    }
+
+}
---
+++ multiple-rpc-core/src/test/java/com/feign/dubbo/example/core/MultipleRpcCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feign.dubbo.example.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleRpcCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
